56c73af6c31638d0537dd949c86a9f1e52c60457
Joe0mran <yousif.edu.omran@gmail.com>
2025-03-29 12:54:25 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,16 @@
 <?php
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    conn = new mysqli('localhost', 'root', '', 'test1');
-    if($conn->connect_error){
-        die('Connection Failed: '.$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into Log-In(email, passwors) values(?, ?)");
-        $stmt->bind_param("ss", $email $password);
-        stmt->excute();
-        echo "login succecfuly..";
+    $conn = new mysqli('localhost', 'root', '', 'test1');
+    if ($conn->connect_error) {
+        die('Connection Failed: ' . $conn->connect_error);
+    }else {
+        $stmt = $conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $email, $hashedPassword);
+        $stmt->execute();
+        echo "Registration successful! Please wait for admin approval.";
         $stmt->close();
         $conn->close();
     }
